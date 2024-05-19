3488912a155cbd2f7d0194ae56e970cabeb5ef86
Hettgen <hettgen@sheridancollege.ca>
2024-05-19 13:53:47 -0400

    Repo made

--- DpsCalc/obj/Debug/net8.0/DpsCalc.AssemblyInfo.cs
+++ DpsCalc/obj/Debug/net8.0/DpsCalc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DpsCalc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3a7983a932184713d592bd210b52a9fd6bcfe53")]
 [assembly: System.Reflection.AssemblyProductAttribute("DpsCalc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DpsCalc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
